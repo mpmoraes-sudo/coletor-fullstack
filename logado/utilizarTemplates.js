90d2e90f17abf31e07d8c0f03967cffde67ae7bb
mpmoraes-sudo <mpmoraes@hcpa.edu.br>
2025-11-25 23:57:43 -0300

    Redirect to ModuloDeUtilizacao.html on missing params

--- logado/utilizarTemplates.js
+++ logado/utilizarTemplates.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   if (!projetoId || !templateId) {
     alert("Projeto ou Template não informados.");
-    window.location.href = "selecaoDeModulos.html";
+    window.location.href = "ModuloDeUtilizacao.html";
     return;
   }
 
